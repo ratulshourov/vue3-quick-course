e098d361065b3c76303aab6ee1e15d0d03291a6c
Md.Ratul Uddin Ashraf <ratul@jrfintel.com>
2023-10-01 16:12:40 +0600

    router work and external css file create and use

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>About Page</h1>
+      <!-- Your about page content goes here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutPage',
+  };
+  </script>
+  
+  <style scoped>
+  /* Your component-specific CSS here */
+  </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1> Page</h1>
+      <!-- Your home page content goes here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+  };
+  </script>
+  
+  <style scoped>
+  /* Your component-specific CSS here */
+  </style>
