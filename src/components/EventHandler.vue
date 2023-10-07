7f50cfa7ac14b3108ca406998a61528e91cdcadb
Md.Ratul Uddin Ashraf <ratul@jrfintel.com>
2023-10-07 14:45:50 +0600

    event handler with parameter example

--- src/components/EventHandler.vue
+++ src/components/EventHandler.vue
@@ -11,14 +11,30 @@
           </div>
         </div>
       </div>
+      
     </div>
+    <div class="row">
+        <div class="col-md-5">
+          <p class="h3">Event Handler With Parameter </p>
+          <div class="card shadow-lg">
+            <div class="card-body">
+              <p class="h5 display-7">{{this.msg}}  Sir</p>
+              <button class="btn btn-primary m-1" @click="greet('Good Morning')">Good Morning </button>
+              <button class="btn btn-success m-1" @click="greet('Good Evening')">Good Evening</button>
+              <button class="btn btn-dark m-1" @click="greet('Good Night')">Good Night</button>
+            </div>
+          </div>
+        </div>
+        
+      </div>
   </div>
 </template>
 <script>
 export default {
   data: function() {
     return {
-        count:0
+        count:0,
+        msg:''
     };
   },
   methods:{
@@ -27,6 +43,9 @@ export default {
     },
     decrement:function() {
         this.count=this.count-1;
+    },
+    greet:function(message) {
+        this.msg=message;
     }
   }
 };
