7b26fa60dbb98f2552258079e8ed5a09236004ed
Md.Ratul Uddin Ashraf <ratul@jrfintel.com>
2023-11-05 15:39:23 +0600

    vuex state set in the  store js

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -2,7 +2,7 @@ import { createStore } from "vuex";
 
 export default createStore ({
     state: {
-        name:'',
-        title:''
+        name:'ratul',
+        title:'vuex state set'
     }
 })
